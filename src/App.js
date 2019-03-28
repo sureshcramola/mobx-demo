7410af7ebb6d6e6c1c0994e10b410c359239760b
sureshramola <suresh.ramola@evontech.com>
2019-03-28 15:44:08 +0530

    fetch based on route parameter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Catalogue from './components/Catalogue';
+import About from './components/About';
+import Contact from './components/Contact';
 import { Provider } from 'mobx-react';
 import ProductStore from './stores/ProductStore';
 import userStore from './stores/UserStore';
@@ -9,11 +12,25 @@ class App extends Component {
   render() {
     return (
       <Provider productStore={ProductStore} userStore={userStore}>
-        <div className="App">
-          <div className="main-wrapper">
-            <Catalogue productStore={ProductStore} />
+        <Router>
+          <div className="App">
+            <div className="main-wrapper">
+
+            <div className="nav-wrapper">
+              <Link  className="nav-links" to="/">Catalogue</Link>
+              <Link  className="nav-links" to="/about/2">About</Link>
+              <Link  className="nav-links" to="/contact">Contact</Link>
+            </div>
+              
+            
+
+              <Route exact path="/" component={Catalogue} />
+              <Route path="/about/:id" component={About} />
+              <Route path="/contact" component={Contact} />
+              {/*  */}
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+ 
+  render() {
+    return (
+      <section className="user-wrapper">
+        <div className="d-flex flex-row-wrap-center">
+          Contact US
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Contact;
